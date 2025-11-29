a0b523133a60fde34e5f3e6f91a653124a38971f
kitproton <dsgsdfgsd@dsbsdhg>
2025-11-29 14:58:28 +0100

    empty backend

---
+++ backend/megabrAIns-hackathon-project/src/test/java/sk/sillyclaws/megabrainshackathonproject/MegabrAInsHackathonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.sillyclaws.megabrainshackathonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegabrAInsHackathonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

e93a96a8040709dfcd987bf2bede46fd15e6c0f1
Aliaksandr Semutsenka <aliaksandr.semutsenka@student.tuke.sk>
2025-11-29 15:32:41 +0100

    Scroll fixed

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,36 +1,31 @@
-import styles from './Map.module.css';
-import {useEffect} from "react";
-import L from "leaflet";
+import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import "leaflet.heat";
 
-function Map({className}) {
+import styles from "./Map.module.css";
+
+function Map({ className }) {
   useEffect(() => {
-    const map = L.map("map").setView([48.72118205130795, 21.2575640746237], 11);
+    const map = L.map("map").setView([48.721182, 21.257564], 13);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
-      .addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-    const points = [
-      [48.721, 21.257, 0.2],
-      [48.722, 21.258, 0.4],
-      [48.723, 21.256, 0.6],
-      [48.720, 21.255, 0.9],
-    ];
+    const marker = L.marker([48.721182, 21.257564]).addTo(map);
+    marker.bindPopup("Košice").openPopup();
 
-    L.marker([48.72118205130795, 21.2575640746237]).addTo(map);
-
-    return () => {
-      map.remove();
-    };
+    return () => map.remove();
   }, []);
 
   return (
     <div
-      className={styles.Map + ' ' + (className || '')}
       id="map"
-    >
-
-    </div>
+      className={`${styles.Map} ${className || ""}`}
+      style={{ width: "100%", height: "100vh" }}
+    />
   );
 }
 

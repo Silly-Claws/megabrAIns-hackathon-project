f09fa0b749f08108c8a496cc42b1f696b4127e5d
kitproton <dsgsdfgsd@dsbsdhg>
2025-11-29 21:45:58 +0100

    backend fix

--- backend/megabrAIns-hackathon-project/src/main/java/sk/sillyclaws/megabrainshackathonproject/config/CoordinatesConfig.java
+++ backend/megabrAIns-hackathon-project/src/main/java/sk/sillyclaws/megabrainshackathonproject/config/CoordinatesConfig.java
@@ -9,5 +9,5 @@ public class CoordinatesConfig {
     public static final Point TOP_LEFT     = new Point(48.759358, 21.187071);
     public static final Point BOTTOM_RIGHT = new Point(48.678284, 21.316946);
 
-    public static final double DISTANCE = 500;
+    public static final double DISTANCE = 100;
 }

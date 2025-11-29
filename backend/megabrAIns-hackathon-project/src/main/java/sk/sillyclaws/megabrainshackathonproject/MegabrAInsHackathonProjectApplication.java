74c4ff5f8f99d59e074f274636adbc520dc237e0
kitproton <dsgsdfgsd@dsbsdhg>
2025-11-29 14:58:28 +0100

    empty backend

---
+++ backend/megabrAIns-hackathon-project/src/main/java/sk/sillyclaws/megabrainshackathonproject/MegabrAInsHackathonProjectApplication.java
@@ -0,0 +1,13 @@
+package sk.sillyclaws.megabrainshackathonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegabrAInsHackathonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegabrAInsHackathonProjectApplication.class, args);
+    }
+
+}
